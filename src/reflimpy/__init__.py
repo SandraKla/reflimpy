c8d4c994e4073c33dc425b35a81e9e8f268484b8
Yihao Han <y.han@ostfalia.de>
2026-08-31 11:02:04 +0200

    Box-Cox-Transformation implementieren und testen

--- src/reflimpy/__init__.py
+++ src/reflimpy/__init__.py
@@ -2,6 +2,16 @@
 
 from ._rounding import adjust_digits
 from ._statistics import bowley
+from ._transformations import (
+    box_cox_transform,
+    inverse_box_cox_transform,
+)
 from ._zlog import zlog
 
-__all__ = ["adjust_digits", "bowley", "zlog"]
+__all__ = [
+    "adjust_digits",
+    "bowley",
+    "box_cox_transform",
+    "inverse_box_cox_transform",
+    "zlog",
+]
